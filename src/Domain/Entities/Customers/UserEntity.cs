fec4b771789e4effdb3670ccc5b932ba46e87961
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2024-09-11 19:10:09 +0330

    🌱 Setup user entity properties.

--- src/Domain/Entities/Customers/UserEntity.cs
+++ src/Domain/Entities/Customers/UserEntity.cs
@@ -2,5 +2,30 @@
 {
     public class UserEntity : BaseEntity
     {
+        public string Username { get; private set; }
+        // TODO:
+        // Password should be hash.
+        public string Password { get; set; }
+
+        #region Relations
+
+
+
+        #endregion
+
+        #region Ctor
+
+        public UserEntity(string username, string password)
+        {
+            if (string.IsNullOrEmpty(username))
+                throw new ArgumentNullException("Username cannot be null or empty.");
+            Username = username;
+
+            if (string.IsNullOrEmpty(password))
+                throw new ArgumentNullException("Password cannot be null or empty.");
+            Password = password;
+        }
+
+        #endregion
     }
 }
